a072b70a0fc73ca3b4f655ffd698db84ad401e12
certainty <david.krentzlin@gmail.com>
2025-01-24 18:26:40 +0100

    Properly configure lspkind

--- lua/morpheus/base/completion.lua
+++ lua/morpheus/base/completion.lua
@@ -44,10 +44,8 @@ return { -- Autocompletion
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
+    local lspkind = require 'lspkind'
     luasnip.config.setup {}
-    local ELLIPSIS_CHAR = '…'
-    local MAX_LABEL_WIDTH = 80
-    local MIN_LABEL_WIDTH = 20
 
     cmp.setup {
       snippet = {
@@ -68,17 +66,25 @@ return { -- Autocompletion
         },
       },
       formatting = {
-        format = function(entry, vim_item)
-          local label = vim_item.abbr
-          local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
-          if truncated_label ~= label then
-            vim_item.abbr = truncated_label .. ELLIPSIS_CHAR
-          elseif string.len(label) < MIN_LABEL_WIDTH then
-            local padding = string.rep(' ', MIN_LABEL_WIDTH - string.len(label))
-            vim_item.abbr = label .. padding
-          end
-          return vim_item
-        end,
+        format = lspkind.cmp_format {
+          mode = 'symbol', -- show only symbol annotations
+          maxwidth = {
+            -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
+            -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+            menu = 50, -- leading text (labelDetails)
+            abbr = 50, -- actual suggestion item
+          },
+          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function(entry, vim_item)
+            -- ...
+            return vim_item
+          end,
+        },
       },
       -- For an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`

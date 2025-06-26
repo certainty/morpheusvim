fdb012d9ee9fd2986cfd5128c4e9bbd62f3c34eb
certainty <david.krentzlin@gmail.com>
2025-06-26 08:32:59 +0200

    Setup gemini

--- lua/ai/codecompanion.lua
+++ lua/ai/codecompanion.lua
@@ -23,6 +23,31 @@ return {
               },
             })
           end,
+          gemini = function()
+            return require('codecompanion.adapters').extend('gemini', {
+              schema = {
+                model = {
+                  default = 'gemini-2.5-flash',
+                },
+              },
+              env = {
+                api_key = 'cmd:op read op://personal/gemini-neovim/credential --no-newline',
+              },
+            })
+          end,
+
+          gemini_pro = function()
+            return require('codecompanion.adapters').extend('gemini', {
+              schema = {
+                model = {
+                  default = 'gemini-2.5-pro',
+                },
+              },
+              env = {
+                api_key = 'cmd:op read op://personal/gemini-neovim/credential --no-newline',
+              },
+            })
+          end,
         },
         display = {
           action_palette = {
@@ -43,6 +68,14 @@ return {
               close = {
                 -- I almost never want to actually close.
                 -- Toggle is what I usually want
+                modes = { n = '<C-q>', i = '<C-q>' },
+              },
+            },
+          },
+          cmd = {
+            adapter = 'anthropic',
+            keymaps = {
+              close = {
                 modes = { n = '<C-q>', i = '<C-q>' },
               },
             },
@@ -71,6 +104,8 @@ return {
       vim.keymap.set({ 'n', 'v' }, '<Leader>a1', '<cmd>CodeCompanionChat anthropic<cr>', { noremap = true, silent = true, desc = ' Chat with Anthropic' })
       vim.keymap.set({ 'n', 'v' }, '<Leader>a2', '<cmd>CodeCompanionChat copilot<cr>', { noremap = true, silent = true, desc = ' Chat with Copilot' })
       vim.keymap.set({ 'n', 'v' }, '<Leader>a3', '<cmd>CodeCompanionChat openai<cr>', { noremap = true, silent = true, desc = ' Chat with OpenAI' })
+      vim.keymap.set({ 'n', 'v' }, '<Leader>a4', '<cmd>CodeCompanionChat gemini<cr>', { noremap = true, silent = true, desc = ' Chat with Gemini-Flash' })
+      vim.keymap.set({ 'n', 'v' }, '<Leader>a5', '<cmd>CodeCompanionChat gemini-pro<cr>', { noremap = true, silent = true, desc = ' Chat with Gemini-Pro' })
 
       -- Expand 'cc' into 'CodeCompanion' in the command line
       vim.cmd [[cab cc CodeCompanion]]

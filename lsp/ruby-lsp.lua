7c44802305abf2170f6c371e6eb48786a71771c5
certainy <david.krentzlin@gmail.com>
2025-10-10 17:14:37 +0200

    more config

---
+++ lsp/ruby-lsp.lua
@@ -0,0 +1,6 @@
+return {
+  cmd = { "ruby-lsp" },
+  filetypes = { "ruby", "eruby", "rakefile", "thorfile", "gemfile", "guardfile", "podfile" },
+  init_options = { formatting = true },
+  root_markers = { ".git", "Gemfile" }
+}
